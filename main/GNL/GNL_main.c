a4f8c2288e84ebf1ae27162c55362571cb7d9376
sm222 <soleil.maraska@gmail.com>
2022-11-29 11:47:57 -0500

    NORM

--- main/GNL/GNL_main.c
+++ main/GNL/GNL_main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   GNL_main.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wdelaros <wdelaros@student.42quebec.com    +#+  +:+       +#+        */
+/*   By: anboisve <anboisve@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 11:04:36 by anboisve          #+#    #+#             */
-/*   Updated: 2022/11/29 11:20:32 by wdelaros         ###   ########.fr       */
+/*   Updated: 2022/11/29 11:46:34 by anboisve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -211,67 +211,58 @@ char	*combine(char *s, ...)
 		}
 	}
 	va_end(list);
-	return(new);
+	return (new);
 }
 
-
-int compare(char *f1, int argc, char *argv[])
+int	compare(char *f1, int argc, char *argv[])
 {
-    FILE *fp1   ;
-    FILE *fp2   ;
-     
-    int cnt1 = 0;
-    int cnt2 = 0;
-    int flg  = 0;
-     
-    fp1 = fopen(f1,"r");
-    if( fp1 == NULL )
-    {
-        printf("\n%s File can not be opened : \n",argv[1]);
-        return -1;
-    }
-     
-    // move file pointer to end and get total number of bytes
-    fseek(fp1,0,SEEK_END);
-    cnt1 = ftell(fp1);
-     
-    fp2 = fopen(argv[argc],"r");
-    if( fp2 == NULL )
-    {
-        printf("\n%s File can not be opened : \n",argv[2]);
-        return -1;
-    }
-     
-    // move file pointer to end and get total number of bytes
-    fseek(fp2,0,SEEK_END);
-    cnt2 = ftell(fp2);
-     
-    fseek(fp1,0,SEEK_SET);
-    fseek(fp2,0,SEEK_SET);
-     
-    // check for the total number of bytes
-    if( cnt1 != cnt2 ){
-        printf(RED"\n[KO]\n"WHT);
-    }
-    else
-    {
-        while( ! feof(fp1) )
-        {
-                if( fgetc(fp1) != fgetc(fp2) )
-                {
-                    flg = 1;
-                    break;
-                }
-        }
-         
-        if( flg )   printf(RED"\n[KO]\n"WHT);
-        else        printf(GRN"\n[OK]\n"WHT);
-    }
- 
-    fclose(fp1);
-    fclose(fp2);
- 
-    return 0;
+	FILE	*fp1;
+	FILE	*fp2;
+	int		cnt1 = 0;
+	int		cnt2 = 0;
+	int		flg = 0;
+
+	fp1 = fopen(f1, "r");
+	if (fp1 == NULL)
+	{
+		printf("\n%s File can not be opened : \n", argv[1]);
+		return (-1);
+	}
+	// move file pointer to end and get total number of bytes
+	fseek(fp1, 0, SEEK_END);
+	cnt1 = ftell(fp1);
+	fp2 = fopen(argv[argc], "r");
+	if (fp2 == NULL)
+	{
+		printf("\n%s File can not be opened : \n", argv[2]);
+		return (-1);
+	}
+	// move file pointer to end and get total number of bytes
+	fseek(fp2, 0, SEEK_END);
+	cnt2 = ftell(fp2);
+	fseek(fp1, 0, SEEK_SET);
+	fseek(fp2, 0, SEEK_SET);
+	// check for the total number of bytes
+	if (cnt1 != cnt2)
+		printf(RED"\n[KO]\n"WHT);
+	else
+	{
+		while (!feof(fp1))
+		{
+			if (fgetc(fp1) != fgetc(fp2))
+			{
+				flg = 1;
+				break ;
+			}
+		}
+		if (flg)
+			printf(RED"\n[KO]\n"WHT);
+		else
+			printf(GRN"\n[OK]\n"WHT);
+	}
+	fclose(fp1);
+	fclose(fp2);
+	return (0);
 }
 
 //
